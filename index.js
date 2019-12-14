0ffacf6e3c4454797a1f8689cb51713f14e75773
MunaLugonvu <munalugonvu@gmail.com>
2019-12-14 17:43:55 +0300

    update

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const newsapi = new NewsAPI('04dcdb75a7d34496ba4fa437ceb8283b')
 var readlineSync = require('readline-sync')
 //et chosenSource, newsSource;
  var newsSource = readlineSync.question('Which news Source would you prefer?\n 1 For BBC news\n 2 For Fox News\n 3 For Aljazzera News\n 4. For BBC Sport News\n');
-let chosenSource,keyword;
+let chosenSource;
+var keyword = readlineSync.question('Any keyword to search for? ');
+
 if(newsSource === '1'){
     chosenSource = 'bbc-news';
    }
@@ -18,15 +20,15 @@ if(newsSource === '1'){
    chosenSource = 'bbc-sport';
    }
    else{
-   console.log('\x1b[31m',"You have to choose a number\n");
+   console.log("Invalid Input\n");
    process.exit()//exit application
    }
-   function articlesarray(prods) {
+   function articlesarray(el) {
     let article_names = [];
-    for (let i=0; i<prods.length; i+=1) {
-    article_names.push('\x1b[33m',i+1 +"."+ prods[i].title);
-    article_names.push('\x1b[34m',prods[i].description);
-    article_names.push('\x1b[32m',prods[i].url);
+    for (let i=0; i<el.length; i+=1) {
+    article_names.push('\x1b[33m',i+1 +"."+ el[i].title);
+    article_names.push('\x1b[34m',el[i].description);
+    article_names.push('\x1b[32m',el[i].url);
     }
     return article_names.join('\r\n');
     }
@@ -45,7 +47,7 @@ if(newsSource === '1'){
     }
     else
     {
-    console.log('\x1b[31m',"No results for that keyword within that time, or try again with the right spelling of the key word");
+    console.log("No results for that keyword within that time, or try again with the right spelling of the key word");
       }
     }
     });
--- index.test.js
+++ index.test.js
@@ -1,1 +1,11 @@
 const require = require('./index');
+test("returns Invalid Input if user doesnot choose from given sources", () =>{
+    expect(newsSource(6)).toBe('Invalid Input');
+});
+    
+test("Enter a number to get news from a corresponding news source ", () => {
+    expect(news_sources(1)).toBe("bbc-news");
+    expect(news_sources(2)).toBe("fox-news");
+    expect(news_sources(3)).toBe("al-jazeera-english");
+    expect(news_sources(4)).toBe("bbc-sport");
+  });
